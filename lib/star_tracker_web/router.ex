a0f1189ca8f0e8dc8108acb72000c33aa79b17e6
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-10 13:44:49 -0500

    intro controller and route

--- lib/star_tracker_web/router.ex
+++ lib/star_tracker_web/router.ex
@@ -17,6 +17,7 @@ defmodule StarTrackerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/info", PageController, :info
   end
 
   # Other scopes may use custom stacks.
